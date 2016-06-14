1cff9c5494c21be3260c47c6b95f49bba234a6e7
Pascal Sturmfels <psturm@umich.edu>
2016-06-14 09:35:41 -0400

    continued work on COS implementation

--- open_shop.cpp
+++ open_shop.cpp
@@ -8,11 +8,13 @@ using namespace std;
 class COS_job {
 public:
     vector<int> processing_times;
-    int weight;
+    double weight;
+    bool scheduled;
 
-    COS_job(const int w, const &vector<int> pi) {
+    COS_job(const double w, &vector<int> pi) {
         processing_times = move(pi);
         weight = w;
+        scheduled = false;
     }
 };
 
@@ -33,8 +35,47 @@ public:
     int num_machines;
     int num_jobs;
     
+    COS_instance(const int N, const int M, &vector<COS_job> j) {
+        num_jobs = N;
+        num_machines = M;
+        jobs = move(j);
+    }
+    
+    
     COS_solution solve() {
+        //Initialization step
         
+        COS_solution sol(num_jobs);
+        vector<double> adj_weights(num_jobs);
+        vector<int> loads(num_machines, 0);
+        for (int i = 0; i < num_machines; ++i) {
+            for (int j = 0; j < num_jobs; ++j) {
+                loads[i] += jobs[j].processing_times[i];
+            }
+        }
+        for (int j = 0; j < num_jobs; ++j) {
+            adj_weights[j] = jobs[j].weight;
+        }
+        
+        //Solve step
+        
+        for (int k = 0; k < num_jobs; ++k) {
+            int max_load = 0;
+            int mu = 0;
+            for (int i = 0; i < num_machines; ++i) {
+                if (max_load < loads[i]) {
+                    max_load = loads[i];
+                    mu = i;
+                }
+            }
+            
+            int min_ratio = 0;
+            int next_job = 0;
+            for 
+            
+            
+            
+        }
         
     }
     
