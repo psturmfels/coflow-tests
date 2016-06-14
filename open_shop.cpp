31656bf1d5a1dc6db31fd781e1bbbcd8fa0c09e5
Pascal Sturmfels <psturm@umich.edu>
2016-06-14 09:49:21 -0400

    now testing COS solution

--- open_shop.cpp
+++ open_shop.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
+#include <limits>
 
 using namespace std;
 
@@ -9,12 +10,12 @@ class COS_job {
 public:
     vector<int> processing_times;
     double weight;
-    bool scheduled;
+    bool is_scheduled;
 
     COS_job(const double w, &vector<int> pi) {
         processing_times = move(pi);
         weight = w;
-        scheduled = false;
+        is_scheduled = false;
     }
 };
 
@@ -24,8 +25,8 @@ public:
     vector<int> completion_times;
     
     COS_solution(int num_jobs) {
-        order.reserve(num_jobs);
-        completion_times.reserve(num_jobs);
+        order.resize(num_jobs);
+        completion_times.resize(num_jobs);
     }
 }
 
@@ -59,7 +60,9 @@ public:
         
         //Solve step
         
-        for (int k = 0; k < num_jobs; ++k) {
+        for (int k = num_jobs - 1; k >= 0; --k) {
+        
+            //Determmine machine with max load
             int max_load = 0;
             int mu = 0;
             for (int i = 0; i < num_machines; ++i) {
@@ -69,15 +72,42 @@ public:
                 }
             }
             
-            int min_ratio = 0;
-            int next_job = 0;
-            for 
             
+            //Determine which job has the smallest
+            //weight-to-processing time ratio, schedule it last
+            double min_ratio = numeric_limits<double>::max(); //theta
+            int next_job = 0; //sigma
+            for (int j = 0; j < num_jobs; ++j) {
+                if (!jobs[j].is_scheduled) {
+                    double ratio = jobs[j].weight / jobs[j].processing_times[mu];
+                    if (min_ratio < ratio) {
+                        min_ratio = ratio;
+                        next_job = j;
+                    }
+                }
+            }
             
+            //Adjust remaining weights
+            for(int j = 0; j < num_jobs; ++j) {
+                if (!jobs[j].is_scheduled) {
+                    jobs[j].weight -= (min_ratio * jobs[j].processing_times[mu]);
+                }
+            }
             
+            //Record solution
+            sol.order[k] = next_job;
+            sol.completion_times[k] = max_load;
+            
+            //Update machine loads
+            for (int i = 0; i < num_machines; ++i) {
+                loads[i] -= jobs[next_job].processing_times[i];
+            }
+            
+            //Update unscheduled jobs
+            jobs[next_job].is_scheduled = true;
         }
         
+        return sol;
     }
-    
 }
 
